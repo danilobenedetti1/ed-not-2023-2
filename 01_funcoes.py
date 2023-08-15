a2d5246bddd8b6fe5e2f2c08da43113e2d32c160
Danilo Benedetti Ribeiro <danilobenedetti1@gmail.com>
2023-08-14 22:13:23 -0300

    14.08.23 aula2

--- 01_funcoes.py
+++ 01_funcoes.py
@@ -1,35 +1,17 @@
-# Função que calcual o IMC da pessoa
-def calc_imc(peso, altura):
-    return peso / altura ** 2
-
-# p = 85
-# a = 1.72
-# imc = calc_imc(p, a)
-
-# print(imc)
-
-print(calc_imc(85, 1.72))
-
-###########################################################################
-
 from math import pi
 
-"""
-    Função que calcula a área de uma figura geométrica plana
-"""
-
 def calc_area(base, altura, tipo):
-    resultado = None
-
-    if tipo == "R":            # Retângulo
+    resultado = None         # Valor inexistente
+    if tipo == "R":          # Retangulo
         resultado = base * altura
-    elif tipo == "T":          # Triângulo
+    elif tipo == "T":        # Triangulo
         resultado = base * altura / 2
-    elif tipo == "E":          # Elipse
-        resultado = (base/2) * (altura / 2) * pi
+    elif tipo == "E":        # Elipse
+        resultado = (base / 2) * (altura / 2) * pi
+    
     return resultado
 
-print("Retângilo 20x30: ", calc_area(20, 30, "R"))
-print("Triângulo 45x32: ", calc_area(45, 32, "T"))
+print("Retangulo 20x30: ", calc_area(20, 30, "R"))
+print("Triangulo 45x32: ", calc_area(45, 32, "T"))
 print("Elipse 10x15: ", calc_area(10, 15, "E"))
-print("Desconhecido 12x16: ", calc_area(12, 16, "X"))
+print("DEsconhecido 12x16: ", calc_area(12, 16, "X"))
---
+++ 02_funcoes.py
@@ -0,0 +1,4 @@
+# uma lista de números
+valores = [2, 3, 5, 7, 9, 11]
+
+#uma lista de valores
